9ef7b1069d0fd9314ce4809ce487e476ea3789b1
divyansha <singhdivyansha243@gmail.com>
2026-04-22 00:27:20 +0530

    Fix API endpoint to use router.huggingface.co/hf-inference

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
+        "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell",
         {
           headers: {
             Authorization: `Bearer ${HF_TOKEN}`,
